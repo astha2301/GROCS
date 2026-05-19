9ab0f697b11102ffe1275d480c9a968f4d1038e0
astha <raiastha525@gmail.com>
2026-05-19 20:24:51 +0530

    Improved TSP algorithm

--- BACKEND/algorithms/tsp.js
+++ BACKEND/algorithms/tsp.js
@@ -14,6 +14,8 @@ function haversine(a, b) {
     return R * 2 * Math.asin(Math.sqrt(x));
 }
 
+
+// Total Distance
 function totalDistance(route) {
 
     let total = 0;
@@ -29,11 +31,48 @@ function totalDistance(route) {
     return total;
 }
 
-function tspRoute(depots, bins) {
 
-    const start = depots[0];
+// Nearest Neighbor Initial Route
+function nearestNeighbor(start, bins) {
 
-    let route = [...bins];
+    const unvisited = [...bins];
+
+    let current = start;
+
+    const route = [start];
+
+    while (unvisited.length > 0) {
+
+        let bestIndex = 0;
+        let shortest = Infinity;
+
+        for (let i = 0; i < unvisited.length; i++) {
+
+            const distance = haversine(
+                current,
+                unvisited[i]
+            );
+
+            if (distance < shortest) {
+
+                shortest = distance;
+                bestIndex = i;
+            }
+        }
+
+        current = unvisited.splice(bestIndex, 1)[0];
+
+        route.push(current);
+    }
+
+    route.push(start);
+
+    return route;
+}
+
+
+// 2-OPT Optimization
+function twoOpt(route) {
 
     let improved = true;
 
@@ -41,30 +80,62 @@ function tspRoute(depots, bins) {
 
         improved = false;
 
-        for (let i = 0; i < route.length - 1; i++) {
+        for (let i = 1; i < route.length - 2; i++) {
 
-            for (let j = i + 1; j < route.length; j++) {
+            for (let j = i + 1; j < route.length - 1; j++) {
 
                 const newRoute = [...route];
+
+                const reversed = newRoute
+                    .slice(i, j + 1)
+                    .reverse();
 
                 newRoute.splice(
                     i,
                     j - i + 1,
-                    ...route.slice(i, j + 1).reverse()
+                    ...reversed
                 );
 
-                if (
-                    totalDistance(newRoute) <
-                    totalDistance(route)
-                ) {
+                const currentDistance =
+                    totalDistance(route);
+
+                const newDistance =
+                    totalDistance(newRoute);
+
+                if (newDistance < currentDistance) {
+
                     route = newRoute;
+
                     improved = true;
                 }
             }
         }
     }
 
-    return [start, ...route, start];
+    return route;
+}
+
+
+// Main TSP Function
+function tspRoute(depots, bins) {
+
+    if (depots.length === 0) {
+        return [];
+    }
+
+    if (bins.length === 0) {
+        return depots;
+    }
+
+    const start = depots[0];
+
+    // Step 1: Create initial route
+    let route = nearestNeighbor(start, bins);
+
+    // Step 2: Optimize route
+    route = twoOpt(route);
+
+    return route;
 }
 
 module.exports = tspRoute;
